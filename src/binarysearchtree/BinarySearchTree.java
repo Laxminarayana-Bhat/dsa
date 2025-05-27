43fe5499b3079ffee504ae22a92d52d9f10ef4eb
Laxminarayana Alekha <laxmishalekha123@gmail.com>
2025-05-28 00:01:42 +0530

    efficient one

--- src/binarysearchtree/BinarySearchTree.java
+++ src/binarysearchtree/BinarySearchTree.java
@@ -379,4 +379,27 @@ public class BinarySearchTree {
         return Math.max(maxDepth(node.left), maxDepth(node.right));
     }
 
+    class kthSmall {
+
+        int count = 0;
+        int ans = -1;
+
+        public int kthSmallest(Node root, int k) {
+            inorder(root, k);
+            return ans;
+        }
+
+        void inorder(Node node, int k) {
+            if (node == null) return;
+            inorder(node.left, k);
+            count++;
+            if (count == k) {
+                ans = node.value;
+                return;
+            }
+
+            inorder(node.right, k);
+        }
+    }
+
 }
