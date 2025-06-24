94cb334c635eac731bb4702703c03861fe8fa04f
Laxminarayana Alekha <laxmishalekha123@gmail.com>
2025-06-24 18:20:00 +0530

    climbing stairs with cost

--- src/dp/DP.java
+++ src/dp/DP.java
@@ -7,7 +7,7 @@ public class DP {
 
     class Solution {
         public int fib(int n) {
-            Integer[] memo = new Integer[31];
+            Integer[] memo = new Integer[n+1];
             return fibHelper(n, memo);
         }
 
@@ -21,7 +21,7 @@ public class DP {
 
     class Solution2 {
         public int climbStairs(int n) {
-            Integer[] memo=new Integer[46];
+            Integer[] memo=new Integer[n+1];
             memo[1]=1;
             memo[2]=2;
             return helper(memo,n);
@@ -33,6 +33,37 @@ public class DP {
             }
             memo[n]=helper(memo,n-1)+helper(memo,n-2);
             return memo[n];
+        }
+    }
+
+    class SolutionTopDown {
+        public int minCostClimbingStairs(int[] cost) {
+            Integer[] memo = new Integer[cost.length + 1];
+            return helper(memo, cost.length, cost);
+        }
+
+        public static int helper(Integer[] memo, int n, int[] cost) {
+            if (n <= 1)
+                return 0;
+            if (memo[n] != null)
+                return memo[n];
+            memo[n] = Math.min(helper(memo, n - 1, cost) + cost[n - 1],
+                    helper(memo, n - 2, cost) + cost[n - 2]);//we should store it else whats the use of memo
+            return memo[n];
+        }
+    }
+
+    class SolutionBottomUp {
+        public int minCostClimbingStairs(int[] cost) {
+            int n=cost.length;
+            int mincost[] = new int[n+1];
+            if(n==0||n==1){
+                return 0;
+            }
+            for(int i=2;i<=n;i++){
+                mincost[i]=Math.min((cost[i-1]+mincost[i-1]),(cost[i-2]+mincost[i-2]));
+            }
+            return mincost[n];
         }
     }
 }
